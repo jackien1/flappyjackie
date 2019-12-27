a33ba3b015e3ebbf67dcc50d915c9832fca3b7f0
jackien1 <jackieni2002@gmail.com>
2019-12-26 20:24:08 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">
+
+<p align="center">
+  <img src="assets/icons/preview.jpeg"/>
+</p>
+
+    Flappy Jackie
+
+Universal Expo App
+
+</h1>
+
+Expo & PIXI.js
---
+++ src/sprites.js
@@ -0,0 +1,8 @@
+export default [
+  { name: "background", x: 0, y: 0, width: 720, height: 960 },
+  { name: "pipe", x: 725, y: 0, width: 130, height: 800 },
+  { name: "bird_000", x: 725, y: 805, width: 85, height: 60 },
+  { name: "bird_001", x: 725, y: 870, width: 85, height: 60 },
+  { name: "bird_002", x: 860, y: 0, width: 85, height: 60 },
+  { name: "ground", x: 860, y: 65, width: 80, height: 360 }
+];
