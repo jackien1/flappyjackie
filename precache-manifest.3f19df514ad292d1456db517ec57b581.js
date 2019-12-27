4a3083ac5b23a4d951c945b35a5d5ce30f45b645
jackien1 <jackieni2002@gmail.com>
2019-12-26 20:25:14 -0800

    Updates

---
+++ precache-manifest.3f19df514ad292d1456db517ec57b581.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79d0a3ca1cce39dd421e65263b6b8a9",
+    "url": "/flappyjackie/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "7e3997674629b9dfe35c",
+    "url": "/flappyjackie/static/js/app.02de1d25.chunk.js"
+  },
+  {
+    "revision": "21467f39a5ec1cd1f1498216cd665577",
+    "url": "/flappyjackie/static/js/app.02de1d25.chunk.js.gz"
+  },
+  {
+    "revision": "84b2e8c5b7d5ed7e1f0035dcc9b2ebf6",
+    "url": "/flappyjackie/static/media/spritesheet.84b2e8c5.png"
+  },
+  {
+    "revision": "9716c086c6eaaf5f2eb8c23f1ca0589b",
+    "url": "/flappyjackie/static/js/runtime~app.a260842d.js.gz"
+  },
+  {
+    "revision": "5c8a3e6cc6b173beb6cf",
+    "url": "/flappyjackie/static/js/runtime~app.a260842d.js"
+  },
+  {
+    "revision": "3d2ef3f39c0f0dc7e1bc55f3c9e46657",
+    "url": "/flappyjackie/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "f1952a8fb82b6ea5c7452779e108930e",
+    "url": "/flappyjackie/static/js/2.8367becc.chunk.js.gz"
+  },
+  {
+    "revision": "2b601c500a037decda9d21d641167280",
+    "url": "/flappyjackie/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/flappyjackie/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "/flappyjackie/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/flappyjackie/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/flappyjackie/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/flappyjackie/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/flappyjackie/expo-service-worker.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/flappyjackie/serve.json"
+  },
+  {
+    "revision": "dbddc3acc7a9a95eea45cefbf452b28d",
+    "url": "/flappyjackie/manifest.json"
+  },
+  {
+    "revision": "5e00832e7bedca40f5b2f50c2e50a4fa",
+    "url": "/flappyjackie/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "95a514e0e712df4b389b",
+    "url": "/flappyjackie/static/js/2.8367becc.chunk.js"
+  },
+  {
+    "revision": "8b9b43c3eee60331e27ecac7c86d9cf2",
+    "url": "/flappyjackie/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "5e1a5afb706e34b96dd7db77c13cfd1f",
+    "url": "/flappyjackie/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "/flappyjackie/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/flappyjackie/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a8469856afcd04f955e3d6e9dfc0159b",
+    "url": "/flappyjackie/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "06a7e9a78092d06ab13691f56e6e4182",
+    "url": "/flappyjackie/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "68a49f4a757ce6b0160144162a5d0e5c",
+    "url": "/flappyjackie/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "96644a808ebdeca819d8cd4b99a2dd82",
+    "url": "/flappyjackie/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "99f16edd3a54a2847073a0a05e974f9f",
+    "url": "/flappyjackie/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "d45c000d2fb1203ce41165a128d2ec17",
+    "url": "/flappyjackie/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "780b91d6bead066902b5f163171c86ec",
+    "url": "/flappyjackie/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d8f60be162f9553820322aa53f4733e",
+    "url": "/flappyjackie/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "2a8905c7457476cc0241a0ecc970ef75",
+    "url": "/flappyjackie/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "390fa02419dc8ff012bbe4afd1a0ed24",
+    "url": "/flappyjackie/index.html"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/flappyjackie/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/flappyjackie/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/flappyjackie/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/flappyjackie/./fonts/AntDesign.ttf"
+  }
+];
